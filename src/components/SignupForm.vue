fed3d72c20cc5e92f5292ae5645951092629a7e7
Mworekwa Ezekiel <vanheaven6@gmail.com>
2021-08-31 23:09:02 -0700

    add form with input field

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,8 +1,44 @@
-<template></template>
+<template>
+  <form>
+    <label>Email:</label>
+    <input type="email" />
+  </form>
+</template>
 
 <script>
-export default {};
+export default {
+  name: "SignupForm",
+};
 </script>
 
 <style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background-color: #fff;
+  text-align: center;
+  padding: 40px;
+  border-radius: 10px;
+  text-align: left;
+}
+
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px auto 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+
+input {
+  display: block;
+  width: 100%;
+  padding: 10px 6px;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
 </style>
